3e8f7ec8530a9f95bc0bb4f1b4a7fabb7f993fd0
Dilip Kushwaha <kush.dilip@gmail.com>
2013-09-19 03:03:10 +0530

    Modified WebSocketTransport.java to make it work with android
    Changes taken from 
    https://github.com/Gottox/socket.io-java-client/issues/60

--- src/io/socket/WebsocketTransport.java
+++ src/io/socket/WebsocketTransport.java
@@ -28,7 +28,20 @@ class WebsocketTransport extends WebSocketClient implements IOTransport {
     public WebsocketTransport(URI uri, IOConnection connection) {
         super(uri);
         this.connection = connection;
-        SSLContext context = IOConnection.getSslContext();
+//        SSLContext context = IOConnection.getSslContext();
+        SSLContext context = null;
+        try {
+            context = SSLContext.getInstance("TLS", "HarmonyJSSE");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } 
+        try {
+            context.init(null, null, null);
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
         if("wss".equals(uri.getScheme()) && context != null) {
 	        this.setWebSocketFactory(new DefaultSSLWebSocketClientFactory(context));
         }
